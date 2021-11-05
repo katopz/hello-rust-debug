0401528c8e8c5b66e8a3bc6fc4e50d76ea34b7fe
katopz <katopz@gmail.com>
2021-11-05 11:36:51 +0700

    Correct debug_anchor

--- tests/hello-rust-debug.ts
+++ tests/hello-rust-debug.ts
@@ -1,13 +1,13 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
-import { HelloRustDebug } from '../target/types/hello_rust_debug';
+import { DebugAnchor } from '../target/types/debug_anchor';
 
-describe('hello-rust-debug', () => {
+describe('debug_anchor', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.HelloRustDebug as Program<HelloRustDebug>;
+  const program = anchor.workspace.DebugAnchor as Program<DebugAnchor>;
 
   it('Is initialized!', async () => {
     // Add your test here.
